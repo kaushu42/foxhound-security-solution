fc6e441f45ef9ad554d132018efd5f05a726025e
Kaushal <kaushu42@gmail.com>
2019-10-14 12:05:41 +0545

    Created db engine, initialized backend

--- foxhound/db_engine/DBEngine.py
+++ foxhound/db_engine/DBEngine.py
@@ -1,2 +1,41 @@
+import os
+from sqlalchemy import create_engine
+import pandas as pd
+
+
 class DBEngine(object):
-    pass
+    def __init__(self, input_dir: str):
+        if not isinstance(input_dir, str):
+            raise TypeError('Input_dir must be a string')
+        self._INPUT_DIR = input_dir
+        self._check_data_dir_valid(self._INPUT_DIR)
+        self._csvs = self._get_csv_paths(self._INPUT_DIR)
+
+    def _read_csv(self, csv: str):
+        df = pd.read_csv(csv)
+        return df
+
+    def _write_to_db(self, csv: str, *, table_name: str):
+
+        DB_ENGINE = create_engine(
+            'postgresql://foxhounduser:foxhound123@localhost:5432/foxhounddb'
+        )
+
+        data = self._read_csv(csv)
+        data.index.name = 'id'
+        data.to_sql(table_name, DB_ENGINE, if_exists='append', index=False)
+
+    def _get_csv_paths(self, path: str):
+        files = os.listdir(path)
+        csvs = [os.path.join(path, f) for f in files if f.endswith('.csv')]
+        return csvs
+
+    def run(self, verbose=False, *, table_name: str):
+        for csv in self._csvs:
+            if verbose:
+                print('Writing to db: ', csv)
+            self._write_to_db(csv, table_name=table_name)
+
+    def _check_data_dir_valid(self, data_dir: str):
+        if not os.path.isdir(data_dir):
+            raise FileNotFoundError('Directory does not exist: ' + data_dir)
--- foxhound/dc_engine/Engine.py
+++ foxhound/dc_engine/Engine.py
@@ -27,24 +27,38 @@ class Engine(ABC):
 
         self._INPUT_TO_OUTPUT_MAP = {}
 
-        self._csvs = self.get_csv_paths()
+        self._input_csvs = self.get_input_csv_paths()
+        self._output_csvs = self.get_output_csv_paths()
 
     @abstractmethod
     def _process(self, data: pd.DataFrame):
         pass
 
     def _check_data_dirs_valid(self):
-        self._check_data_dir_valid(self._INPUT_PATH)
-        self._check_data_dir_valid(self._OUTPUT_PATH)
+        self._check_data_dir_valid(self._INPUT_PATH, directory_type='Input')
+        self._check_data_dir_valid(self._OUTPUT_PATH, directory_type='Output')
         return True
 
-    def _check_data_dir_valid(self, data_dir: str):
+    def _check_data_dir_valid(self, data_dir: str, directory_type: str):
         if not os.path.isdir(data_dir):
-            raise FileNotFoundError('Directory does not exist: ' + data_dir)
+            raise FileNotFoundError(
+                directory_type + ' directory does not exist: ' + data_dir)
 
-    def get_csv_paths(self):
-        files = os.listdir(self._INPUT_PATH)
-        csvs = [os.path.join(self._INPUT_PATH, f) for f in files]
+    def get_input_csv_paths(self):
+        # files = os.listdir(self._INPUT_PATH)
+        # csvs = [os.path.join(self._INPUT_PATH, f) for f in files]
+        # return csvs
+        return self._get_csv_paths(self._INPUT_PATH)
+
+    def get_output_csv_paths(self):
+        # files = os.listdir(self._OUTPUT_PATH)
+        # csvs = [os.path.join(self._OUTPUT_PATH, f) for f in files]
+        # return csvs
+        return self._get_csv_paths(self._OUTPUT_PATH)
+
+    def _get_csv_paths(self, path: str):
+        files = os.listdir(path)
+        csvs = [os.path.join(path, f) for f in files if f.endswith('.csv')]
         return csvs
 
     def _read_csv(self, csv_path: str):
@@ -53,15 +67,29 @@ class Engine(ABC):
     def _dump(self, input_filename: str, processed_data: pd.DataFrame):
         filename = input_filename.split('/')[-1]
         output_filename = self._OUTPUT_PATH + '/OUTPUT_' + filename
-        processed_data.to_csv(output_filename)
+        processed_data.to_csv(output_filename, index=False)
 
-    def _run_for_one(self, csv_path: str):
+    def _run_one(self, csv_path: str):
         data = self._read_csv(csv_path)
         data = self._process(data)
-        self._dump(csv, data)
+        self._dump(csv_path, data)
 
     def run(self, verbose=False):
-        for csv in self._csvs:
+        # for csv in self._input_csvs:
+        #     if verbose:
+        #         print('Processing:', csv)
+        #     self._run_one(csv)
+        self._run(self._run_one, self._input_csvs, verbose=verbose)
+
+    def _delete_one(self, csv_path: str):
+        print(csv_path)
+
+    def _delete_csv_files(self, verbose=False):
+        self._run(lambda x: x, self._output_csvs,
+                  verbose=verbose, message='Deleted:')
+
+    def _run(self, callback, csvs, verbose=False, message='Processing:'):
+        for csv in csvs:
             if verbose:
-                print('Processing:', csv)
-            self._run_for_one(csv)
+                print(message, csv)
+            callback(csv)
