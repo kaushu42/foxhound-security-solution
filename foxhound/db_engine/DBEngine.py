8ec731a94e92700452cd75e73ff73f15f262d6fd
Kaushal <kaushu42@gmail.com>
2019-10-19 16:58:14 +0545

    Created models for the database using SQLAlchemy. The DB Engine has been modified to use these models to write data to database. The writing to TrafficLog table is done using SQLalchemy model. The writing to TrafficLogDetail table is done by pandas as before.

--- foxhound/db_engine/DBEngine.py
+++ foxhound/db_engine/DBEngine.py
@@ -1,38 +1,96 @@
 import os
 import re
+import datetime
 
-from sqlalchemy import create_engine
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+
 import pandas as pd
 
 
+from .core_models import VirtualSystem, TrafficLog, TrafficLogDetail
+
+
 class DBEngine(object):
-    def __init__(self, input_dir: str):
+    def __init__(self, input_dir: str, *, db_engine):
         if not isinstance(input_dir, str):
             raise TypeError('Input_dir must be a string')
+
+        if not isinstance(db_engine, sqlalchemy.engine.base.Engine):
+            raise TypeError('db_engine must be an sqlalchemy engine instance')
+
         self._INPUT_DIR = input_dir
+        self._db_engine = db_engine
         self._check_data_dir_valid(self._INPUT_DIR)
         self._csvs = self._get_csv_paths(self._INPUT_DIR)
+        self._session = sessionmaker(bind=db_engine)
 
     def _read_csv(self, csv: str):
         df = pd.read_csv(csv)
         return df
 
-    def _write_to_db(self, csv: str, *, table_name: str):
-        db_name = os.environ.get('FH_DB_NAME', '')
-        db_user = os.environ.get('FH_DB_USER', '')
-        db_password = os.environ.get('FH_DB_PASSWORD', '')
-        DB_ENGINE = create_engine(
-            f'postgresql://{db_user}:{db_password}@localhost:5432/{db_name}'
+    def _get_virtual_system(self, data, session):
+        # data should have only 1 virtual system
+        vsys = data['virtual_system_id'].unique()
+        assert(len(vsys) == 1)
+        vsys = session.query(VirtualSystem).filter_by(code=vsys[0])[0]
+        return vsys
+
+    def _get_traffic_log(self, csv, vsys, session):
+        query = {
+            'virtual_system_id': vsys.id,
+            'processed_datetime': datetime.datetime.now(),
+            'log_date': self._get_date(csv),
+            'log_name': self._get_filename(csv)
+        }
+
+        traffic_log = TrafficLog(**query)
+
+        # Flush changes to db to get the newly inserted id
+        session.add(traffic_log)
+        session.flush()
+        session.commit()
+        return traffic_log
+
+    def _write_to_traffic_log_detail(self, data, traffic_log):
+        data.rename(
+            columns={
+                'virtual_system_id': 'traffic_log_id'
+            },
+            inplace=True
+        )
+        data['traffic_log_id'] = traffic_log.id
+        data.to_sql(
+            TrafficLogDetail.__tablename__,
+            self._db_engine,
+            if_exists='append',
+            index=False
         )
 
+    def _write_to_db(self, csv: str, *, table_name: str):
         data = self._read_csv(csv)
-        data.index.name = 'id'
-        self._add_date_column(data, csv)
-        data.to_sql(table_name, DB_ENGINE, if_exists='append', index=False)
+
+        session = self._session()
+
+        # Get the virtual system id from database
+        vsys = self._get_virtual_system(data, session)
+        # Use the key to write the data into the core_trafficlog table
+        traffic_log = self._get_traffic_log(csv, vsys, session)
+        # Write to the core_trafficlogdetail using the obtained traffic_log id
+        self._write_to_traffic_log_detail(data, traffic_log)
+
+    def _get_filename(self, string):
+        processed_filename = string.split('/')[-1]
+        filename = processed_filename.split('_vsys')[0] + '.csv'
+        return filename
+
+    def _get_date(self, string):
+        date = re.findall(r'[0-9]{4}_[0-9]{2}_[0-9]{2}',
+                          string)[0].replace('_', '-')
+        return date
 
     def _add_date_column(self, data, csv):
-        date = re.findall(r'[0-9]{4}_[0-9]{2}_[0-9]{2}',
-                          csv)[0].replace('_', '-')
+
         data['date'] = pd.to_datetime(date)
 
     def _get_csv_paths(self, path: str):
---
+++ foxhound/db_engine/core_models.py
@@ -0,0 +1,118 @@
+from sqlalchemy import Column, Integer, String, BigInteger, Boolean
+from sqlalchemy import Date, DateTime, ForeignKey, UniqueConstraint
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class VirtualSystem(Base):
+    __tablename__ = 'core_virtualsystem'
+
+    id = Column(Integer, primary_key=True)
+    code = Column(String)
+    name = Column(String)
+    domain_code = Column(String)
+    tenant_name = Column(String)
+    domain_url = Column(String)
+
+    def __repr__(self):
+        return self.name
+
+
+class User(Base):
+    __tablename__ = 'users_foxhounduser'
+    __table_args__ = (UniqueConstraint('username', name='_username_uc'),)
+
+    id = Column(Integer, primary_key=True)
+    password = Column(String)
+    last_login = Column(DateTime)
+    is_superuser = Column(Boolean)
+    username = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String)
+    is_staff = Column(Boolean)
+    is_active = Column(Boolean)
+    date_joined = Column(DateTime)
+    tenant_id = Column(Integer)
+
+    def __repr__(self):
+        return self.username
+
+    def __str__(self):
+        return self.__repr__()
+
+
+class TrafficLog(Base):
+    __tablename__ = 'core_trafficlog'
+
+    id = Column(Integer, primary_key=True)
+    virtual_system_id = Column(
+        ForeignKey(
+            VirtualSystem.id,
+            ondelete='CASCADE'
+        )
+    )
+    processed_datetime = Column(DateTime)
+    log_date = Column(DateTime)
+    log_name = Column(String)
+
+    def __repr__(self):
+        return self.log_name
+
+
+class TrafficLogDetail(Base):
+    __tablename__ = 'core_trafficlogdetail'
+
+    id = Column(Integer, primary_key=True)
+    traffic_log_id = Column(ForeignKey(TrafficLog.id, ondelete='CASCADE'))
+    source_ip = Column(String)
+    source_port = Column(Integer)
+    destination_ip = Column(String)
+    destination_port = Column(Integer)
+    bytes_sent = Column(BigInteger)
+    bytes_received = Column(BigInteger)
+    repeat_count = Column(Integer)
+    application = Column(String)
+    packets_received = Column(BigInteger)
+    packets_sent = Column(BigInteger)
+    protocol = Column(String)
+    time_elapsed = Column(BigInteger)
+    source_zone = Column(String)
+    destination_zone = Column(String)
+    firewall_rule = Column(String)
+
+    def __repr__(self):
+        return f'Log-{self.date}'
+
+    def __str__(self):
+        return self.__repr__()
+
+
+class RuleDictionary(Base):
+    __tablename__ = 'core_ruledictionary'
+
+    id = Column(Integer, primary_key=True)
+    virtual_system_id = Column(
+        ForeignKey(
+            VirtualSystem.id,
+            ondelete='CASCADE'
+        )
+    )
+    verified_by_user_id = Column(ForeignKey(User.id, ondelete='CASCADE'))
+    created_date_time = Column(DateTime)
+    rule_name = Column(String)
+    source_ip = Column(String)
+    destination_ip = Column(String)
+    application = Column(String)
+    rule_description = Column(String)
+    is_verified_rule = Column(Boolean)
+
+    verified_date_time = Column(DateTime)
+
+    def __repr__(self):
+        return self.rule_name
+
+    def __str__(self):
+        return self.__repr__()
