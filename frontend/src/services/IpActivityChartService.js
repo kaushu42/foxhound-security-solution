4bb8b3e036640cea645568ddf86c65626c4c97e9
keshavchaurasia <keshav.chaurasia1995@gmail.com>
2019-10-23 20:02:35 +0545

    service folder added

---
+++ frontend/src/services/IpActivityChartService.js
@@ -0,0 +1,9 @@
+import {ROOT_URL} from "../utils";
+
+const FETCH_URL = `${ROOT_URL}`
+
+export const asyncIpActivityChartService = ip_address => {
+    const data = null;
+
+    return data;
+}
---
+++ frontend/src/utils.js
@@ -0,0 +1,1 @@
+export const ROOT_URL = "https://192.168.100.14/api/v1/"
