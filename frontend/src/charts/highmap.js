b312b847a8219ac9267eda6e808fe7d663a1bf71
banjarajanardan <janardanb0@gmail.com>
2019-10-30 16:48:14 +0545

    world map added

---
+++ frontend/src/charts/highmap.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import mapdata from "./mapdata";
+
+// Load Highcharts modules
+require("highcharts/modules/map")(Highcharts);
+
+class HighMap extends React.Component {
+render() {
+const data = this.props.data;
+
+const mapOptions = {
+  title: {
+    text: ""
+  },
+  colorAxis: {
+    min: 0,
+    stops: [[0.1, "#cccccc"],[1, "#000000"]]
+  },
+
+  series: [
+    {
+      mapData: mapdata,
+      name: "World",
+      data: data
+    }
+  ]
+};
+
+// Render app with demo chart
+
+  
+    return (
+      <div>
+        <HighchartsReact
+          options={mapOptions}
+          constructorType={"mapChart"}
+          highcharts={Highcharts}
+        />
+      </div>
+    );
+  }
+}
+
+export default HighMap;
