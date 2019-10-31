8d45932e0c3e10c86871c85ad36df310c5194f89
Kaushal <kaushu42@gmail.com>
2019-10-31 10:33:13 +0545

    cors test on react

--- frontend/src/services/filterSelectDataService.jsx
+++ frontend/src/services/filterSelectDataService.jsx
@@ -1,18 +1,23 @@
-import {ROOT_URL} from "../utils";
+import { ROOT_URL } from "../utils";
 import axios from "axios";
 
-const FETCH_API = `${ROOT_URL}dashboard/filters/`
+const FETCH_API = `${ROOT_URL}dashboard/filters/`;
 
-export const filterSelectDataServiceAsync = (auth_token) => {
+export const filterSelectDataServiceAsync = auth_token => {
+  const authorization = `Token ${auth_token}`;
 
-    const authorization = `Token ${auth_token}`;
-
-    let headers = {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: authorization
-    };
-    return axios.post(FETCH_API, {},{
-        headers: headers
-    });
-}
+  let headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Headers": "*",
+    Authorization: authorization
+  };
+  return axios.post(
+    FETCH_API,
+    {},
+    {
+      headers: headers
+    }
+  );
+};
