465ceec08bab857fe12d867673bce65af6ce884d
Kaushal <kaushu42@gmail.com>
2019-11-01 16:41:41 +0545

    DBEngine now creates the ipcountry table with ip and its country code

--- scripts/main.py
+++ scripts/main.py
@@ -20,51 +20,6 @@ db_engine = create_engine(
 Session = sessionmaker(bind=db_engine)
 session = Session()
 
-
-def is_ip_private(ip: str):
-    if (
-        ip.startswith('192.168.') or
-        ip.startswith('10.') or
-        ip.startswith('172.16') or
-        ip.startswith('172.17') or
-        ip.startswith('172.18') or
-        ip.startswith('172.19') or
-        ip.startswith('172.2') or
-        ip.startswith('172.30.') or
-        ip.startswith('172.31.')
-    ):
-        return True
-    return False
-
-
-def save_country_in_db():
-    import geoip2.database
-    reader = geoip2.database.Reader('./GeoLite2-City.mmdb')
-
-    ips = set()
-    for ip in TrafficLogDetail.objects.values_list('source_ip'):
-        ips.add(ip[0])
-    for ip in TrafficLogDetail.objects.values_list('destination_ip'):
-        ips.add(ip[0])
-    for ip in ips:
-        ip_country = IPCountry(ip=ip)
-        country_name = ''
-        country_iso_code = ''
-        if is_ip_private(ip) is not True:
-            country = reader.city(ip).country
-            country_iso_code = country.iso_code
-            country_name = country.name
-            if country_iso_code is None:
-                country_name = 'Unknown'
-                country_iso_code = '---'
-        else:
-            country_iso_code = "np"
-            country_name = "Nepal"
-        ip_country.country_name = country_name
-        ip_country.country_iso_code = country_iso_code
-        ip_country.save()
-
-
 # Seed the database
 if session.query(VirtualSystem).count() == 0:
     vsys1 = VirtualSystem(
@@ -94,12 +49,12 @@ if session.query(VirtualSystem).count() == 0:
     session.commit()
     session.close()
 
-pa = fh.dc_engine.PaloAltoEngine(
-    config.TRAFFIC_LOGS_INPUT_DIR, config.TRAFFIC_LOGS_OUTPUT_DIR)
-pa.run(verbose=True)
+# pa = fh.dc_engine.PaloAltoEngine(
+#     config.TRAFFIC_LOGS_INPUT_DIR, config.TRAFFIC_LOGS_OUTPUT_DIR)
+# pa.run(verbose=True)
 
-db = fh.db_engine.DBEngine(config.TRAFFIC_LOGS_OUTPUT_DIR, db_engine=db_engine)
-db.run(verbose=True)
+# db = fh.db_engine.DBEngine(config.TRAFFIC_LOGS_OUTPUT_DIR, db_engine=db_engine)
+# db.run(verbose=True)
 
 
 init = Initialize(config.TRAFFIC_LOGS_INPUT_DIR, config.IP_PROFILE_OUTPUT_DIR)
