68883b74781db02ebc7ace2a68a889fe8b142d60
Kaushal <kaushu42@gmail.com>
2019-11-07 23:18:10 +0545

    Recreated login API

--- backend/serializers/serializers.py
+++ backend/serializers/serializers.py
@@ -9,15 +9,16 @@ from users.models import FoxhoundUser
 
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = FoxhoundUser
-        fields = ('username', 'tenant_id', 'id')
+        fields = ('username', 'id')
 
 
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True)
-    tenant_id = serializers.IntegerField(required=False)
+    domain_url = serializers.CharField(required=True)
 
 
 class TrafficLogSerializer(serializers.ModelSerializer):
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
@@ -9,8 +11,15 @@ from rest_framework.status import (
     HTTP_401_UNAUTHORIZED
 )
 from rest_framework.response import Response
+from core.models import Domain
+
 from serializers.serializers import UserSerializer, UserLoginSerializer
 from .auth import token_expire_handler, expires_in
+
+_ERROR_MESSAGE = Response(
+    {'detail': 'Invalid Credentials or inactive account'},
+    status=HTTP_401_UNAUTHORIZED
+)
 
 
 @api_view(["POST"])
@@ -20,16 +29,19 @@ def login(request):
     if not login_serializer.is_valid():
         return Response(login_serializer.errors, status=HTTP_400_BAD_REQUEST)
 
+    domain_url = request.data.get('domain_url')
+    try:
+        domain_name = urlparse(domain_url).hostname.split('.')[0]
+        tenant_id = Domain.objects.get(name=domain_name).tenant.id
+    except Domain.DoesNotExist as e:
+        return _ERROR_MESSAGE
+
     user = authenticate(
         username=login_serializer.data['username'],
         password=login_serializer.data['password'],
-        tenant_id=0  # Get tenant_id from url
     )
-    if not user:
-        return Response(
-            {'detail': 'Invalid Credentials or activate account'},
-            status=HTTP_401_UNAUTHORIZED
-        )
+    if (not user) or (tenant_id != user.tenant_id):
+        return _ERROR_MESSAGE
 
     # TOKEN STUFF
     token, _ = Token.objects.get_or_create(user=user)
@@ -41,14 +53,7 @@ def login(request):
 
     return Response({
         'user': user_serialized.data,
-        'expires_in': expires_in(token),
-        'token': token.key
-    }, status=HTTP_200_OK)
-
-
-@api_view(["GET"])
-def user_info(request):
-    return Response({
-        'user': request.user.username,
-        'expires_in': expires_in(request.auth)
+        'token': token.key,
+        'full_name': f'{user.first_name} {user.last_name}',
+        'tenant_name': user.tenant.name
     }, status=HTTP_200_OK)
