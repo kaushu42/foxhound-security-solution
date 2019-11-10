2287640ff657e93bb67c5cd74a4a7339d3d779bd
Kaushal <kaushu42@gmail.com>
2019-11-10 21:53:26 +0545

    Changed the order of running the engines: MLEngine first then dc engine then db engine

--- scripts/main.py
+++ scripts/main.py
@@ -56,6 +56,10 @@ session = Session()
 
 seedutils.seed(session)
 
+mle = MLEngine(config.IP_PROFILE_OUTPUT_DIR, config.IP_MODEL_OUTPUT_DIR,
+               config.TRAFFIC_LOGS_INPUT_DIR, config.ANOMALY_LOGS_OUTPUT_DIR)
+mle.run(create_model=True, predict=True)
+
 pa = fh.dc_engine.PaloAltoEngine(
     config.TRAFFIC_LOGS_INPUT_DIR, config.TRAFFIC_LOGS_OUTPUT_DIR)
 pa.run(verbose=True)
@@ -65,16 +69,10 @@ db = fh.db_engine.DBEngine(
     db_engine=db_engine,
     db_path=os.path.join(config.BASE_PATH, 'GeoLite2-City.mmdb')
 )
-
 db.run(verbose=True)
-
 
 init = Initialize(config.TRAFFIC_LOGS_INPUT_DIR, config.IP_PROFILE_OUTPUT_DIR)
 init.parse_all_csv()
-
-mle = MLEngine(config.IP_PROFILE_OUTPUT_DIR, config.IP_MODEL_OUTPUT_DIR,
-               config.TRAFFIC_LOGS_INPUT_DIR, config.ANOMALY_LOGS_OUTPUT_DIR)
-mle.run(create_model=True, predict=True)
 
 # # tt_anomaly = TTAnomaly(config.ANOMALY_LOGS_OUTPUT_DIR, db_engine)
 # # tt_anomaly.run()
