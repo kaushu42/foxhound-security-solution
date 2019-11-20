79feaed7268cf04a053d02a6f3bf4f565c5afc7a
Kaushal <kaushu42@gmail.com>
2019-11-20 09:29:41 +0545

    Dashboard api country error fix?

--- backend/dashboard_api/views.py
+++ backend/dashboard_api/views.py
@@ -205,9 +205,9 @@ class WorldMapApiView(APIView):
         import os
         import pandas as pd
         from sqlalchemy import create_engine
-        db_name = os.environ.get('FH_DB_NAME', '')
-        db_user = os.environ.get('FH_DB_USER', '')
-        db_password = os.environ.get('FH_DB_PASSWORD', '')
+        db_name = os.environ.get('FH_DB_NAME', 'fhdb')
+        db_user = os.environ.get('FH_DB_USER', 'foxhounduser')
+        db_password = os.environ.get('FH_DB_PASSWORD', 'foxhound123')
         db_engine = create_engine(
             f'postgresql://{db_user}:{db_password}@localhost:5432/{db_name}'
         )
