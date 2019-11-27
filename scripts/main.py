b5b5a641e2c06a9722b732e974837dbcece6e5ba
Prakash Chaudhary <connectwithprakash@yahoo.com>
2019-11-27 12:19:30 +0530

    [Change]: Passing mode for parsing data in parse_all_csv() method
    Incorporating main.py with changes in Initialize.py

--- scripts/main.py
+++ scripts/main.py
@@ -68,13 +68,13 @@ try:
     session = Session()
 
     seedutils.seed(session)
-    init = Initialize(config.TRAFFIC_LOGS_INPUT_DIR,
-                      config.TENANT_PROFILE_OUTPUT_DIR)
-    init.parse_all_csv()
+    # init = Initialize(config.TRAFFIC_LOGS_INPUT_DIR,
+    #                   config.TENANT_PROFILE_OUTPUT_DIR)
+    # init.parse_all_csv('tenant')
 
     mle = MLEngine(config.TENANT_PROFILE_OUTPUT_DIR, config.TENANT_MODEL_OUTPUT_DIR,
                    config.TRAFFIC_LOGS_INPUT_DIR, config.ANOMALY_LOGS_OUTPUT_DIR)
-    mle.run(create_model=True, predict=True)
+    mle.run(create_model=False, predict=True)
     logging.info('DC Engine running')
     pa = fh.dc_engine.PaloAltoEngine(
         config.TRAFFIC_LOGS_INPUT_DIR, config.TRAFFIC_LOGS_OUTPUT_DIR,
