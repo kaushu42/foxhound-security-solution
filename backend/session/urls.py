9041f4f96160aaf5d779e4085088b36b637e7243
Kaushal <kaushu42@gmail.com>
2019-12-03 17:47:22 +0545

    Added token validation api

--- backend/session/urls.py
+++ backend/session/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('tenant/', views.TenantInfoApiView.as_view()),
-    path('user/', views.InfoApiView.as_view())
+    path('user/', views.InfoApiView.as_view()),
+    path('is_valid/', views.IsTokenValidApiView.as_view())
 ]
