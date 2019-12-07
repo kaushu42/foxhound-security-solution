ca9587d3f2c81b493f13f86270714084bbfc3554
banjarajanardan <janardanb0@gmail.com>
2019-12-07 22:09:23 +0545

    update rule bug fixed

--- frontend/src/components/tables/UnverifiedRulesTable.jsx
+++ frontend/src/components/tables/UnverifiedRulesTable.jsx
@@ -179,10 +179,11 @@ class UnverifiedRulesTable extends Component {
     handleUpdateRuleSubmit = (e) => {
         e.preventDefault();
         const {auth_token,unverifiedRulePagination} = this.props;
-        const source_ip = this.source_ip.value;
-        const destination_ip = this.destination_ip.value;
-        const application = this.application.value;
-        const description = this.description.value;
+        const source_ip = this.source_ip.state.value;
+        const destination_ip = this.destination_ip.state.value;
+        const application = this.application.state.value;
+        const description = this.description.state.value;
+        // console.log("details at update info ",source_ip, destination_ip, application, description)
         this.props.dispatchUpdateRule(auth_token,source_ip,destination_ip,application,description,{}, unverifiedRulePagination);
     }
 
