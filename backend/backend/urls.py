acc9e242a6985aca571be3d8ddb0f6d248df1f42
keshavchaurasia <keshav.chaurasia1995@gmail.com>
2019-12-09 21:09:56 +0545

    Spark initsession singleton class created and showed it's usage in core site..

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from core import views
 urlpatterns = [
+	path('',views.index),
     path('admin/', admin.site.urls),
     path('api/v1/', include('apiv1.urls'))
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from spark import Spark
+
 
 # Create your views here.
+def index(request):
+	ss,sc,sqlc = Spark.initSparkSession()
+	return HttpResponse("<h1>Hello Spark!</h1>")
+
