3ad7ccd95c65cb5f4ae16c0c0b0c221663ed8286
Kaushal <kaushu42@gmail.com>
2019-12-26 01:17:17 +0545

    spark error

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,10 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from spark import Spark
+# rom spark import Spark
 
 
 # Create your views here.
 def index(request):
-	ss,sc,sqlc = Spark.initSparkSession()
-	return HttpResponse("<h1>Hello Spark!</h1>")
-
+    return HttpResponse("<h1>Hello foxhound</h1>")
