b66afa53de97b8abb597e2e263eff6246eeaa402
Kaushal <kaushu42@gmail.com>
2020-01-27 22:57:47 +0545

    Added bytes_sent, bytes_received, packets_sent, packets_received and count columns to all charts

--- foxhound/chart_engine/ApplicationChart.py
+++ foxhound/chart_engine/ApplicationChart.py
@@ -12,20 +12,37 @@ class ApplicationChart(BaseChart):
                             'logged_datetime', 'application_id')
 
         # Calculate sum of bytes
-        grouped_df = groups.agg({'bytes_sent': 'sum', 'bytes_received': 'sum'})
+        grouped_df = groups.agg({
+            'bytes_sent': 'sum',
+            'bytes_received': 'sum',
+            'packets_sent': 'sum',
+            'packets_received': 'sum',
+            'application_id': 'count'
+        })
 
-        grouped_df = grouped_df.withColumn(
-            'bytes',
-            grouped_df['sum(bytes_sent)'] + grouped_df['sum(bytes_received)']
+        grouped_df = grouped_df.withColumnRenamed(
+            'sum(bytes_sent)', 'bytes_sent'
+        ).withColumnRenamed(
+            'sum(packets_sent)', 'packets_sent'
+        ).withColumnRenamed(
+            'sum(bytes_received)', 'bytes_received'
+        ).withColumnRenamed(
+            'sum(packets_received)', 'packets_received'
+        ).withColumnRenamed(
+            'count(application_id)', 'count'
         )
-
-        # Cast column from float to int
-        grouped_df = grouped_df.withColumn(
-            'bytes', grouped_df['bytes'].cast("int"))
+        # # Cast column from float to int
+        # grouped_df = grouped_df.withColumn(
+        #     'bytes', grouped_df['bytes'].cast("int"))
 
         # Select only relevant columns
-        grouped_df = grouped_df[['firewall_rule_id',
-                                 'logged_datetime', 'application_id', 'bytes']]
+        grouped_df = grouped_df[[
+            'firewall_rule_id',
+            'logged_datetime', 'application_id',
+            'bytes_sent', 'bytes_received',
+            'packets_sent', 'packets_received',
+            'count'
+        ]]
 
         # Write to db
         self._write_df_to_postgres(grouped_df, 'core_applicationchart')
