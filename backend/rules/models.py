8120a1b57c71247c958c53b7367456bad1080d7a
Keshav Chaurasia <keshavchaurasia1995@gmail.com>
2020-04-18 20:50:33 +0545

    daily traffic log engine created

--- backend/rules/models.py
+++ backend/rules/models.py
@@ -21,3 +21,27 @@ class Rule(models.Model):
         blank=True
     )
     verified_date_time = models.DateTimeField(auto_now=True, null=True)
+
+
+###### STAGE TABLE #################################################################################################
+class StageTrafficRule(models.Model):
+    firewall_rule = models.ForeignKey(
+        FirewallRule, on_delete=models.CASCADE, null=True)
+    created_date_time = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=250)
+    source_address = models.CharField(max_length=50, null=True)
+    destination_address = models.CharField(max_length=50, null=True)
+    application = models.CharField(max_length=50, null=True)
+    description = models.CharField(max_length=250, null=True, blank=True)
+    is_verified_rule = models.BooleanField(default=False)
+    is_anomalous_rule = models.BooleanField(default=False)
+    verified_by_user = models.ForeignKey(
+        FoxhoundUser,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    verified_date_time = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        db_table = 'fh_stg_trfc_rule_f'
