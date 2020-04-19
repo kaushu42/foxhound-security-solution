c07e16eca3bb23eb33fc7d5155f1914508593e22
Keshav Chaurasia <keshavchaurasia1995@gmail.com>
2020-04-19 18:32:35 +0545

    clean project

--- scripts/dailyBatchRun.py
+++ scripts/dailyBatchRun.py
@@ -5,7 +5,6 @@ import dailyBatch as daily_batch_run
 # Ready for staging
 daily_batch_run.ready_for_staging()
 
-
 for input_traffic_log in os.listdir(TRAFFIC_LOGS_INPUT_DIR):
    input_traffic_log = os.path.join(TRAFFIC_LOGS_INPUT_DIR,input_traffic_log)
    if ((not daily_batch_run.is_traffic_log_already_processed(input_traffic_log)) and input_traffic_log.endswith(".csv")):
@@ -20,7 +19,6 @@ for input_threat_log in os.listdir(THREAT_LOGS_INPUT_DIR):
    if ((not daily_batch_run.is_threat_log_already_processed(input_threat_log)) and input_threat_log.endswith(".csv")):
       # sending complete log path to engine 
       daily_batch_run.threat_log_engine(input_threat_log)
-
 
 
 # Changes commit to production
