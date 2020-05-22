a06517a526d073f17ce74169f6c545b6548878e9
KESHAV CHAURASIA <keshav.chaurasia1995@gmail.com>
2020-05-23 00:31:51 +0545

    logger removed in production

--- frontend/src/configureStore.js
+++ frontend/src/configureStore.js
@@ -1,12 +1,22 @@
 import { applyMiddleware, compose, createStore } from 'redux';
-import thunkMiddleware,{thunk} from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk';
 import loggerMiddleware from "./middleware/logger";
 import monitorReducerEnhancer from "./enhancers/monitorReducer";
 import rootReducer from "./rootReducer";
 
 
 export default function configureStore(preloadedState) {
-    const middlewares = [loggerMiddleware, thunkMiddleware]
+    
+    let middleware;
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+        // dev code
+        middleware = [loggerMiddleware, thunkMiddleware]
+      } else {
+        // production code
+        middleware = [thunkMiddleware]
+    }
+
+    const middlewares = middleware
     const middlewareEnhancer = applyMiddleware(...middlewares)
 
     const enhancers = [middlewareEnhancer, monitorReducerEnhancer]
@@ -14,7 +24,6 @@ export default function configureStore(preloadedState) {
 
 
     const store = createStore(rootReducer, preloadedState, composedEnhancers);
-    const dev_store = createStore(rootReducer, applyMiddleware(thunk));
 
     return store;
 }
