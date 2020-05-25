6e62acf7c111d9999ef47e38777cbc17b2ebc41c
Prakash Chaudhary <connectwithprakash@gmail.com>
2020-05-26 02:58:47 +0545

    [New]: required_columns variable added to filter desired column during prediction
    Faster temporary file creation for prediction and faster read with less number of columns to read

--- foxhound/ml_engine/variables.py
+++ foxhound/ml_engine/variables.py
@@ -1,4 +1,12 @@
 # row_number,virtual_system_id,source_ip_id,source_port,destination_ip_id,destination_port,bytes_sent,bytes_received,repeat_count,application_id,packets_received,packets_sent,protocol_id,time_elapsed,source_zone_id,destination_zone_id,firewall_rule_id,logged_datetime,inbound_interface_id,outbound_interface_id,action_id,category_id,session_end_reason_id
+required_columns = ['Time Logged', 'Threat/Content Type', 'Source address', 'Destination address', 'NAT Source IP',
+                    'NAT Destination IP', 'Application', 'Log Action',
+                    'NAT Destination Port', 'Rule', 'Flags', 'IP Protocol', 'Source Zone', 'Destination Zone',
+                    'Inbound Interface', 'Outbound Interface', 'Action', 'Category',
+                    'Session End Reason',  'Destination Port', 'Source Port',
+                    'Bytes Sent', 'Bytes Received', 'Repeat Count', 'Packets Received',
+                    'Packets Sent', 'Start Time', 'Elapsed Time (sec)', 'Virtual System', 'Device Name']
+
 features_list = ['Time Logged',
                  'Rule',
                  'Source address',
