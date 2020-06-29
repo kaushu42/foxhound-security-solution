c45556065f0b41f1c66781b4292ba1aac2951589
kaushu42 <kaushu42@gmail.com>
2020-06-29 23:55:55 +0545

    Created follow up model, migrations, serializers

--- backend/troubleticket/admin.py
+++ backend/troubleticket/admin.py
@@ -4,3 +4,5 @@ from . import models
 
 admin.site.register(models.TroubleTicketAnomaly)
 admin.site.register(models.TroubleTicketFollowUpAnomaly)
+admin.site.register(models.TroubleTicketGroupAnomaly)
+admin.site.register(models.TroubleTicketGroupFollowUpAnomaly)
